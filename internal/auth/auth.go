11008851ff2ce4596fef0f37c2678ffcaf01c576
Notowlmenot <menot229@gmail.com>
2025-04-26 02:55:41 +0700

    Продумал идею авторизации + немного изенил гитигнор

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,9 +1,36 @@
 package auth
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func AuthHandler(c *gin.Context) {
 
+	/* Принцып авторизации:
+	Отправка get-запроса с входными данными login/pasword
+	Поиск в базе данных пользователя
+	сравнение хешированного пароля (через bcrypt?) с введеным.
+	В случае удачной авторизации - выдача пользователю JWT (сохранение его в куки)
+	*/
+
+	//Максимально тупая авторизация без хеширования
+	password := "idk"
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		fmt.Println("Пароль не верен")
+	} else {
+		fmt.Println("Пароль верен")
+	}
+	//Использовать JWT токен для авторизации
+	c.JSON(http.StatusOK, gin.H{"token": "JWT_TOKEN"})
 }
